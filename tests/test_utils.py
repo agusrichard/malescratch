9a541d6249be0bbfabc4ab354bfee40a2c2a1e70
Agus Richard <agus.richard21@gmail.com>
2022-09-20 21:34:43 +0700

    i promise to make this activity into a routine tomorrow

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,4 @@
 def test_something():
     print("I really want to write some tests, but...")
     print("I'll start this routine tomorrow. Wish me luck")
+    print("I'll do that tomorrow. I promise")
