5221c5387392e5ea04bebf8c7d5155e5e2357a16
Agus Richard <agus.richard21@gmail.com>
2022-09-24 22:17:14 +0700

    grand design tomorrow

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,3 +4,6 @@ def test_something():
     print("I'll do that tomorrow. I promise")
     print("I hate myself today!")
     print("I hurt myself today!")
+    print(
+        "This would be the last week of me being stupid. I'll make a (sort of) grand design tomorrow!"
+    )
