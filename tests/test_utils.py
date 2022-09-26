09e46cc3c96e9f07cdf290f9573e1884cfa59ffc
Agus Richard <agus.richard21@gmail.com>
2022-09-26 19:16:11 +0700

    ready to write tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,2 @@
 def test_something():
-    print("I really want to write some tests, but...")
-    print("I'll start this routine tomorrow. Wish me luck")
-    print("I'll do that tomorrow. I promise")
-    print("I hate myself today!")
-    print("I hurt myself today!")
-    print(
-        "This would be the last week of me being stupid. I'll make a (sort of) grand design tomorrow!"
-    )
-    print("The mission of getting back on your feet so you can help yourself and others is started now!")
+    print("I'm ready to write tests for let's create a plan to work on this")
