10d1871fa7f3373f6a323f8815eb1e1284c26680
Agus Richard <agus.richard21@gmail.com>
2022-09-28 21:33:50 +0700

    add more imports

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,10 @@
-from malescratch.utils import train_test_split
+from malescratch.utils import (
+    MinMaxScaler,
+    StandardScaler,
+    to_categorical,
+    train_test_split,
+    make_batch_index,
+)
 
 
 def test_positive_train_test_split():
