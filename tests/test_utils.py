a3dc7bd897b69c0c6041f9732fe3860f131ecae6
Agus Richard <agus.richard21@gmail.com>
2022-10-04 23:25:42 +0700

    first test ever

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from malescratch.utils import (
     MinMaxScaler,
     StandardScaler,
@@ -8,10 +10,14 @@ from malescratch.utils import (
 
 
 def test_positive_train_test_split():
-    print("I need to make plan")
-    print("I'm making plan tonight")
-    print("I've set you on my calendar. Congratulations!")
-    pass
+    X1 = np.random.randint(0, 10, size=(100, 10))
+    y1 = np.random.randint(0, 10, size=(100,))
+    X1_train, X1_test, y1_train, y1_test = train_test_split(X1, y1)
+
+    assert type(X1_train) == np.ndarray
+    assert type(X1_test) == np.ndarray
+    assert type(y1_train) == np.ndarray
+    assert type(y1_test) == np.ndarray
 
 
 def test_positive_make_batch_index():
